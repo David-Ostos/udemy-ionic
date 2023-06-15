18d521ca173ff014a90460e85a9a887ba6fe9844
David <davidostos2@gmail.com>
2023-06-15 11:24:59 -0400

    Agregando proyecto de prueba de pluging

--- Prueba-pluging/capacitor.config.ts
+++ Prueba-pluging/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.counter.app',
-  appName: 'Counter',
+  appId: 'io.ionic.starter',
+  appName: 'Prueba-pluging',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
---
+++ Prueba-pluging/src/views/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <img :src="imageURL"/>
+      <button @click="makePhoto">Take Photo</button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { Camera, CameraResultType } from '@capacitor/camera'
+import { ref } from 'vue';
+
+const imageURL = ref()
+
+const makePhoto = async () => {
+  const image = await Camera.getPhoto({
+    quality: 80,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  })
+  
+  imageURL.value = image.webPath
+
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
